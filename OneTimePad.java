0b2b17abc83b806c5aaf369208ed9e1e54a73905
K. V. M. S. LOKESH <142161415+K-V-M-S@users.noreply.github.com>
2024-03-21 06:20:47 +0530

    Update and rename Exp2.java to OneTimePad.java

--- OneTimePad.java
+++ OneTimePad.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exp2 {
+public class OneTimePad {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Plain Text:");
