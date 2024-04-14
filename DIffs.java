49c1dd6ccb4aed427c80bc45458f998d5e074270
K. V. M. S. LOKESH <142161415+K-V-M-S@users.noreply.github.com>
2024-04-14 12:32:46 +0530

    Add files via upload

---
+++ DIffs.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.util.Random;
+import java.io.*;
+
+public class DIffs {
+    public static void main(String[] args) throws Exception{
+        ServerSocket ss = new ServerSocket(8988);
+        Socket server = ss.accept();
+        Random r = new Random();
+
+        DataInputStream in = new DataInputStream(server.getInputStream());
+        int p = in.readInt();
+        int g = in.readInt();
+        int x = in.readInt();
+        int b = r.nextInt(g);
+        System.out.println("Private key : "+b);
+        int y = (int)(Math.pow(g, b) % p);
+
+        DataOutputStream out = new DataOutputStream(server.getOutputStream());
+        out.writeInt(y);
+
+        int key = (int)(Math.pow(x, b) % p);
+        System.out.println("Secret Key: " + key);
+    }
+}
---
+++ Diffc.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Diffc {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter prime and generator : ");
+        int p = sc.nextInt();
+        int g = sc.nextInt();
+        Random r = new Random();
+
+        int a = r.nextInt(g);
+        System.out.println("Private key : "+a);
+        int x = (int)(Math.pow(g, a) % p);
+
+        Socket client = new Socket("localhost",8988);
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        out.writeInt(p);
+        out.writeInt(g);
+        out.writeInt(x);
+
+        DataInputStream in = new DataInputStream(client.getInputStream());
+        int y = in.readInt();
+
+        int key = (int)(Math.pow(y, a) % p);
+        System.out.println("Secret Key : "+key);
+    }
+}
