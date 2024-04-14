bc5d7b0031c51a465759f8024558f67804729168
K. V. M. S. LOKESH <142161415+K-V-M-S@users.noreply.github.com>
2024-04-14 12:35:40 +0530

    Update DiffieHellman.java

--- DiffieHellman.java
+++ DiffieHellman.java
@@ -1,37 +1,32 @@
-import java.math.BigInteger;
-import java.security.SecureRandom;
-
+import java.util.Scanner;
+import java.security.*;
+import java.math.*;
 public class DiffieHellman {
-    
-    private static final BigInteger P = new BigInteger("6277101735386680763835789423207666416083908700390324961279");
-    private static final BigInteger G = new BigInteger("2");
-    private static final int BIT_LENGTH = 1024;
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SecureRandom r = new SecureRandom();
 
-        BigInteger aPrivate = generatePrivateKey();
-        BigInteger bPrivate = generatePrivateKey();
-        BigInteger aPublic = calculatePublicKey(aPrivate);
-        BigInteger bPublic = calculatePublicKey(bPrivate);
-        BigInteger sharedKeyA = calculateSharedKey(aPrivate, bPublic);
-        BigInteger sharedKeyB = calculateSharedKey(bPrivate, aPublic);
+        System.out.println("Enter prime and generator : ");
+        String prime = sc.next();
+        String gen = sc.next();
 
-        if (sharedKeyA.equals(sharedKeyB))
-            System.out.println("Shared secret key: " + sharedKeyA);
-        else
-            System.out.println("Key exchange failed.");
-    }
+        BigInteger p = new BigInteger(prime);
+        BigInteger g = new BigInteger(gen);
 
-    private static BigInteger generatePrivateKey() {
-        SecureRandom random = new SecureRandom();
-        return new BigInteger(BIT_LENGTH, random);
-    }
+        BigInteger a = new BigInteger(1024,r);
+        BigInteger b = new BigInteger(1024,r);
 
-    private static BigInteger calculatePublicKey(BigInteger privateKey) {
-        return G.modPow(privateKey,P);
-    }
+        BigInteger x = g.modPow(a,p);
+        BigInteger y = g.modPow(b,p);
 
-    private static BigInteger calculateSharedKey(BigInteger privateKey, BigInteger publicKey) {
-        return publicKey.modPow(privateKey,P);
+        BigInteger k1 = y.modPow(a,p);
+        BigInteger k2 = x.modPow(b,p);
+
+        if(k1.compareTo(k2) == 0 && !k1.equals(BigInteger.ZERO)){
+            System.out.println(k1 + " " + k2);
+        }
+        else{
+            System.out.println("Key Exchange failed");
+        }
     }
 }
